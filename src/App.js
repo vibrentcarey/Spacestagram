a136392094ca2dc9c6fcc8e7c87150f93fac33ad
Brent Carey <69613439+brentcareycode@users.noreply.github.com>
2022-01-08 12:20:24 -0800

    Setup api call

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 
 function App() {
+  const API_KEY = process.env.REACT_APP_API_KEY
+  console.log(process.env);
+  const [images, setImages] = useState([])
+  useEffect(() => {
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=${API_KEY}&count=10`)
+      .then(res => {
+        const imagesInfo = res.data.map(image => {
+          return { title: image.title, url: image.url, date: image.date }
+        })
+        setImages(imagesInfo)
+      })
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {images.length > 0 && <h1>{images[0].title}</h1>}
     </div>
   );
 }
