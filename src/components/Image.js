186cb5b72a1899687534c1bb9380addb0b041835
Brent Carey <69613439+brentcareycode@users.noreply.github.com>
2022-01-08 13:32:42 -0800

    Add like functionality

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,12 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 import classes from './image.module.css'
+import { HeartIcon } from '@heroicons/react/outline'
 
 const Image = ({ title, url, date }) => {
+  const [liked, setLiked] = useState(false)
+  
+  const handleLike = () => {
+    setLiked(!liked);
+  }
   return (
     <article className={classes.card}>
-      <h2>Spacestagram </h2>
+      <h2 className={classes.title}>Spacestagram </h2>
       <img className={classes.image} src={url} alt='Space Image' />
+      <p className={classes.content}>{title}</p>
+      <p className={classes.content}>{date}</p>
+      <HeartIcon className={liked ? classes.liked : classes.like} onClick={handleLike} />
     </article>
   );
 };
